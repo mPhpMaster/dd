6472ead4418193835a8f4de9e8df2bbd2ccba56e
mphpmaster <mphpmaster@gmail.com>
2024-04-07 23:24:57 +0300

    v1.1.3

--- src/helper.php
+++ src/helper.php
@@ -1,11 +1,13 @@
 <?php
 
-if (!function_exists('dd')) {
-    function dd()
+if (!function_exists('d2')) {
+    function d2()
     {
 	    $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5);
 	    $d = end($backtrace);
-	    $d['class'] = ($d['class'] ?? null) ? "{$d['class']}::" : '';
+	    $d['file'] = ($d['file'] ?? null) ? "{$d['file']}\n" : '';
+	    $d['class'] = $d['file'].(($d['class'] ?? null) ? "{$d['class']}::" : '');
+
 	    $d['function'] = ($d['function'] ?? null) ? "{$d['function']}()" : '';
 	    $d['line'] = ($d['line'] ?? null) ? "line {$d['line']}" : '';
 	    $d = trim("{$d['class']}{$d['function']} {$d['line']}");
@@ -22,11 +24,13 @@ if (!function_exists('d')) {
     {
 	    $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5);
 	    $d = end($backtrace);
-        $d['class'] = ($d['class'] ?? null) ? "{$d['class']}::" : '';
-        $d['function'] = ($d['function'] ?? null) ? "{$d['function']}()" : '';
-        $d['line'] = ($d['line'] ?? null) ? "line {$d['line']}" : '';
-        $d = trim("{$d['class']}{$d['function']} {$d['line']}");
-        echo "\n<pre>{$d}\n\n</pre>\n\n";
+	    $d['file'] = ($d['file'] ?? null) ? "{$d['file']}\n" : '';
+	    $d['class'] = $d['file'].(($d['class'] ?? null) ? "{$d['class']}::" : '');
+
+	    $d['function'] = ($d['function'] ?? null) ? "{$d['function']}()" : '';
+	    $d['line'] = ($d['line'] ?? null) ? "line {$d['line']}" : '';
+	    $d = trim("{$d['class']}{$d['function']} {$d['line']}");
+	    echo "\n<pre>{$d}\n\n</pre>\n\n";
 
         $args = func_get_args();
         call_user_func_array('dump', $args);
