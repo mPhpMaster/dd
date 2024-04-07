a97e6e65cce5e3b1ae497b2b9c94cccd436bc35a
Moh. Php Master <59211285+mPhpMaster@users.noreply.github.com>
2024-04-07 22:45:10 +0300

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -3,6 +3,14 @@
 if (!function_exists('dd')) {
     function dd()
     {
+        $d = collect(last(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5)))
+            ->only([ 'class', 'function', 'line' ]);
+        $d['class'] = ($d['class'] ?? null) ? "{$d['class']}::" : '';
+        $d['function'] = ($d['function'] ?? null) ? "{$d['function']}()" : '';
+        $d['line'] = ($d['line'] ?? null) ? "line {$d['line']}" : '';
+        $d = trim("{$d['class']}{$d['function']} {$d['line']}");
+        echo "\n<pre>{$d}\n\n</pre>\n\n";
+    
         $args = func_get_args();
         call_user_func_array('dump', $args);
         die();
@@ -12,6 +20,14 @@ if (!function_exists('dd')) {
 if (!function_exists('d')) {
     function d()
     {
+        $d = collect(last(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 5)))
+            ->only([ 'class', 'function', 'line' ]);
+        $d['class'] = ($d['class'] ?? null) ? "{$d['class']}::" : '';
+        $d['function'] = ($d['function'] ?? null) ? "{$d['function']}()" : '';
+        $d['line'] = ($d['line'] ?? null) ? "line {$d['line']}" : '';
+        $d = trim("{$d['class']}{$d['function']} {$d['line']}");
+        echo "\n<pre>{$d}\n\n</pre>\n\n";
+    
         $args = func_get_args();
         call_user_func_array('dump', $args);
     }
